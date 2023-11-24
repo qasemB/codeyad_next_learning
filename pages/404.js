85b69032183cdac00471e7920eec52feb2a5176d
qasemB <mahdicmptr@gmail.com>
2023-11-24 22:42:47 +0330

    part#13-B7

--- pages/404.js
+++
@@ -1,5 +0,0 @@
-export default function Custom404() {
-    return <h1>
-        My 404 page...
-    </h1>
-}
--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            header
-            <Component {...pageProps} />
-            footer
-        </>
-    )
-}
