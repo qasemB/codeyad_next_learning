5633bbe8eca1653beaebe20efd7adf5a871b9b51
qasemB <mahdicmptr@gmail.com>
2023-12-01 13:31:01 +0330

    part#17-B12

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 
 const register = () => {
     return (
-        <h1>
-           Register... 
-        </h1>
+        <div className="container" style={{
+            height: " 80vh",
+            display: " flex",
+            justifyContent: " center",
+            alignItems: " center",
+            flexDirection:"column"
+        }}>
+            <h1 style={{marginBottom:"20px"}}>
+                Register form
+            </h1>
+            <form>
+                <div class="input-group" style={{marginBottom:"20px"}}>
+                    <span class="input-group-addon" id="sizing-addon2">@</span>
+                    <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon2" />
+                </div>
+                <div class="input-group" style={{marginBottom:"20px"}}>
+                    <span class="input-group-addon" id="sizing-addon2">@</span>
+                    <input type="password" class="form-control" placeholder="Password" aria-describedby="sizing-addon2" />
+                </div>
+                <div class="input-group" style={{marginBottom:"20px"}}>
+                    <span class="input-group-addon" id="sizing-addon2">@</span>
+                    <input type="password" class="form-control" placeholder="Confirm Password" aria-describedby="sizing-addon2" />
+                </div>
+
+                <div style={{textAlign:"center"}}>
+                    <button className='btn btn-primary'>Submit</button>
+                </div>
+            </form>
+        </div>
     );
 }
 
