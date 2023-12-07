3f7f460220fe8e1264d18b70e53536cc433f00cc
qasemB <mahdicmptr@gmail.com>
2023-12-07 16:54:09 +0330

    part#21-B16

--- app/cars/layout.jsx
+++ app/cars/layout.jsx
@@ -8,27 +8,6 @@ const CarsLayout = ({ children }) => {
     return (
         <>
             {children}
-            <div
-                style={{
-                    position: "fixed",
-                    width: "90px",
-                    height: "90px",
-                    background: "purple",
-                    zIndex: "1000",
-                    top: "100px",
-                    left: "10px",
-                    color: "white",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: "100%",
-                    flexDirection: "column",
-                    boxShadow: "0 0 5px gray",
-                    cursor: "pointer"
-                }}>
-                <i className='fa fa-shopping-basket'></i>
-                My Cart
-            </div>
         </>
     );
 }
