80b35c15969175fb22bd21438994c7ff96eada71
qasemB <mahdicmptr@gmail.com>
2023-12-07 18:12:22 +0330

    part#22-B17

---
+++ app/loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div 
+        className='container' 
+        style={{width:"100%", height:"100vh", display:"flex", justifyContent:"center", alignItems:"center"}}
+        >
+            Loading...
+        </div>
+    );
+}
+
+export default Loading;
