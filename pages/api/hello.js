3087984bd7811349f20213c048d4bcb843bf14da
qasemB <mahdicmptr@gmail.com>
2024-01-30 18:04:54 +0330

    part#46-E3

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  
-  res.status(200).json({ name: "John Doe" });
+  if(req.method !== "POST"){
+    return res.status(405).json("Method not allowed");
+  }
+  // add new data...  
+  res.status(201).json({ name: "John Doe" });
 }
