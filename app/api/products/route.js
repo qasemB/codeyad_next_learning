d8867767eb6710a7258291e76dc999c94fa5a5d6
qasemB <mahdicmptr@gmail.com>
2024-02-02 15:54:48 +0330

    part#48-E5

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,15 +1,23 @@
 export async function GET(request) {
     const data = [
-        {id: 1, title: "product1", price: 1_000_000},
-        {id: 2, title: "product2", price: 2_000_000},
+        { id: 1, title: "product1", price: 1_000_000 },
+        { id: 2, title: "product2", price: 2_000_000 },
     ]
 
-    return Response.json({ data })
+    return Response.json({ data }
+    //     , {
+    //     headers: {
+    //         'Access-Control-Allow-Origin': '*',
+    //         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    //         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    //     },
+    // }
+    )
 }
 
 export async function POST(request) {
     const data = [
-        {id: 1, title: "product1", price: 1_000_000},
+        { id: 1, title: "product1", price: 1_000_000 },
     ]
 
     return Response.json({ data }, {
