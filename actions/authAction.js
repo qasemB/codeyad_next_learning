9c065761c0bf06f690db8c0e78908497f280fbec
qasemB <mahdicmptr@gmail.com>
2024-05-04 15:27:37 +0330

    part#54-G6

--- actions/authAction.js
+++ actions/authAction.js
@@ -3,12 +3,32 @@
 import axios from "axios"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import { z } from "zod"
+
+const validationSchema = z.object({
+    phone: z.string().min(1, "ورود اطلاعات این فیلد اجباری است") ,
+    password: z.string().min(1, "ورود اطلاعات این فیلد اجباری است")
+})
 
 export const loginAction = async (prevState,formData) => {
     const phone = formData.get("phone")
     const password = formData.get("password")
     const remember = formData.get("remember") || 0
-    const res = await axios.post("https://ecomadminapi.azhadev.ir/api/auth/login", { phone, password, remember })
+
+    const params = { phone, password, remember }
+
+    const validationFields = validationSchema.safeParse(params)
+
+    if (!validationFields.success) {
+        const response = {
+            errors: validationFields.error?.flatten().fieldErrors,
+            success: false
+        }
+
+        return response
+    }
+
+    const res = await axios.post("https://ecomadminapi.azhadev.ir/api/auth/login", params)
     if (res.status === 200) {
         const token = res.data.token
         cookies().set("loginToken", token)
--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -8,6 +8,7 @@ import { useFormState } from 'react-dom'
 
 const Login = () => {
     const [state, formAction] = useFormState(loginAction, {error: "", success: false})
+    console.log(state);
     return (
         <div className='flex justify-center items-center h-full w-full px-2  lg:px-10'>
             <form action={formAction} className='w-full text-gray-600 bg-gradient-to-b from-gray-300 to-gray-100 p-3 rounded-lg border-2 shadow'>
@@ -18,10 +19,12 @@ const Login = () => {
                 <div>
                     <label>شماره موبایل:</label>
                     <input name="phone" type='number' className='px-4 w-full border-gray-300 border-2 rounded-full h-12 focus:outline-blue-300' />
+                    <small className='text-red-500 block'>{state.errors?.phone?.at(0) || ""}</small>
                 </div>
                 <div className='mt-5'>
                     <label>رمز عبور:</label>
                     <input name="password" type='password' className='px-4 w-full border-gray-300 border-2 rounded-full h-12 focus:outline-blue-300' />
+                    <small className='text-red-500 block'>{state.errors?.password.at(0) || ""}</small>
                 </div>
                 <div className='mt-5'>
                     <ToggleBtn value={1} name="remember" title="مرا بخاطر بسپار" />
